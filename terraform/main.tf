2e73597b7b27ba711c8af36eae3a7356b061128c
Vwake04 <baraiyavivek04@gmail.com>
2024-06-07 13:16:17 +0530

    Updated workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,16 +7,9 @@ terraform {
   }
 }
 
-provider "aws" {
-  region     = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
-}
-
-
-
 
 data "aws_caller_identity" "current" {}
+
 
 // Create EC2 instance within the subnet
 resource "aws_instance" "fastapi_instance" {
