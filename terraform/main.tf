c94679306590338f6a3e60a62f4e931c87ca83d5
Vwake04 <baraiyavivek04@gmail.com>
2024-06-07 13:32:56 +0530

    Updated terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,12 +11,12 @@ terraform {
 data "aws_caller_identity" "current" {}
 
 
-// Create EC2 instance within the subnet
-resource "aws_instance" "fastapi_instance" {
-  ami           = "ami-05e00961530ae1b55" // Your desired AMI ID
-  instance_type = "t2.micro"
-  tags = {
-    Name = "fastapi"
-  }
-}
+# // Create EC2 instance within the subnet
+# resource "aws_instance" "fastapi_instance" {
+#   ami           = "ami-05e00961530ae1b55" // Your desired AMI ID
+#   instance_type = "t2.micro"
+#   tags = {
+#     Name = "fastapi"
+#   }
+# }
 
